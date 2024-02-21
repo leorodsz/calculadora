f04fb0684d7ba1b669effe93e8ee59975cd4077d
Léo Rodrigues <rleo3421@gmail.com>
2024-02-21 18:39:50 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿int num1;
+int num2;
+int res;
+string escolha;
+string op;
+Console.WriteLine("Calculadora desenvolvido por Lélo");
+Console.WriteLine("====================================");
+Console.WriteLine("[A] - Adição");
+Console.WriteLine("[B] - Subtração");
+Console.WriteLine("[C] - Multiplicação");
+Console.WriteLine("[B] - Divisão");
+Console.WriteLine("Digite uma opção: ");
+escolha = Convert.ToString(Console.ReadLine());
+Console.WriteLine("======================================");    
+
+if (escolha == "A")
+{ 
+    Console.WriteLine("Voce escolheu adição!");
+    Console.WriteLine("Digite o primeiro numero: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    res = num1 + num2;
+    Console.WriteLine("A resposta do calculo eh: {0}", res);
+}
+
+else if (escolha == "B")
+{
+    Console.WriteLine("Voce escolheu subtração!");
+    Console.WriteLine("Digite o primeiro numero: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    res = num1 - num2;
+    Console.WriteLine("A resposta do calculo eh: {0}", res);
+}
+
+else if (escolha == "C")
+{
+    Console.WriteLine("Voce escolheu multiplicação!");
+    Console.WriteLine("Digite o primeiro numero: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    res = num1 * num2;
+    Console.WriteLine("A resposta do calculo eh: {0}", res);
+}
+
+else if (escolha == "D")
+{
+    Console.WriteLine("Voce escolheu Divisão!");
+    Console.WriteLine("Digite o primeiro numero: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Digite o segundo numero: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    res = num1 / num2;
+    Console.WriteLine("A resposta do calculo eh: {0}", res);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# calculadora
+ Calculadora desenvolvida em c#, com intuito de treinamento a lógica de programação
